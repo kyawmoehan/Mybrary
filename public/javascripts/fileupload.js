e01edc24e4b90ca10a13766e42f819ba2ff5501e
kyawmoehan <kyawmoehan012@gmail.com>
2020-05-27 21:46:03 +0630

    finish front-end style

--- public/javascripts/fileupload.js
+++ public/javascripts/fileupload.js
@@ -1,13 +1,28 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-);
+const rootStyle = window.getComputedStyle(document.documentElement);
 
-FilePond.setOptions({
-        stylePanelAspectRatio: 150/100,
-        imageResizeTargetWidth: 100,
-        imageResizeTargetHeight: 150
-});
+if(rootStyle.getPropertyValue('--book-cover-width-large') != null && rootStyle.getPropertyValue('--book-cover-width-large') !== '') {
+    ready();    
+} else {
+    document.getElementById('mian-css').addEventListener('load', ready);
+}
 
-FilePond.parse(document.body);
+function ready() {
+    const coverWidth = parseFloat(rootStyle.getPropertyValue('--book-cover-width-large'));
+    const coverRatio = parseFloat(rootStyle.getPropertyValue('--book-cover-aspect-ratio'));
+    const coverHeight = coverWidth / coverRatio;   
+
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+    );
+    
+    FilePond.setOptions({
+            stylePanelAspectRatio: 1/coverRatio,
+            imageResizeTargetWidth: coverWidth,
+            imageResizeTargetHeight: coverHeight
+    });
+    
+    FilePond.parse(document.body);
+}
+
